fe375f8b5cd3c46902fd6648865ed7248b456641
TomMao-CN <53568911+TomMao-CN@users.noreply.github.com>
2020-01-10 17:14:56 +0800

    Add files via upload

---
+++ WebServer/WebServer/Controllers/Controllers/ExceptionLogController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Common.Expand;
+
+namespace WebServer.Controllers
+{
+    public class ExceptionLogController : SuperController
+    {
+        #region 异常日志列表
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="startTime">开始时间</param>
+        /// <param name="endTime">结束时间</param>
+        /// <param name="page">页码</param>
+        /// <returns></returns>
+        public JsonResult GetExceptionLogList(string startTime, string endTime, int page = 1)
+        {
+            object result = new object();      
+            try
+            {
+             
+                DateTime? sTime = Common.ConvertData.StrToDateTime(startTime);
+                DateTime? eTime = Common.ConvertData.StrToDateTime(endTime);
+
+                if (sTime == null)
+                    sTime = Models.SharedData.MinTime;
+                if (eTime == null)
+                    eTime = Models.SharedData.MaxTime;
+
+                List<Models.ExceptionLog> exceptionLogs = dataContext.ExceptionLog.Where(m => m.Time >= sTime && m.Time < eTime).ToList();
+
+                int pageSize = 10;
+                int pagePre = page * pageSize - pageSize;
+                int totalCount = exceptionLogs.Count;
+                int mod = totalCount % pageSize;
+                int pageCout = totalCount / pageSize + (mod == 0 ? 0 : 1);
+                int totalPage = totalCount / pageSize + (totalCount % pageSize == 0 ? 0 : 1);
+
+                exceptionLogs = exceptionLogs.OrderByDescending(m => m.Time).Skip(pagePre).Take(pageSize).ToList();
+                List<object> list = new List<object>();
+
+                if (exceptionLogs.Count > 0)
+                {
+                    foreach (var exceptionLog in exceptionLogs)
+                    {
+                        list.Add(new
+                        {
+                            exceptionLog.ID,
+                            exceptionLog.Source,
+                            exceptionLog.Message,
+                            exceptionLog.StackTrace,
+                            Time = Common.ConvertData.ConvertDateTime(exceptionLog.Time, 2)
+                        });
+                    }
+                }
+                result = new
+                {
+                    list,
+                    page,
+                    totalCount,
+                    totalPage,
+                    pageSize
+                };
+            }
+            catch (Exception ex)
+            {
+                errorCode = 10001;
+                new BLL.ServiceException().AddExceptionLog(ex);
+                error = new BLL.ServiceError().GetErrorInfo(errorCode);
+            }
+            return Json(errorCode, error, result, true);
+        }
+        #endregion
+    }
+}
---
+++ WebServer/WebServer/Controllers/Controllers/OtherController/SharedController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebServer.Controllers
+{
+    public class SharedController : OpenController
+    {
+        #region 管理员登陆
+        public JsonResult LoginSystem(string name, string password)
+        {
+            int errorCode = 0;
+            string error = string.Empty;
+            object result = null;
+            try
+            {
+                Models.Administrator administrator = dataContext.Administrator.Where(m => m.Name == name && m.Status == (int)Models.Enums.RoleStatus.Normal).FirstOrDefault();
+                if (administrator != null)
+                {
+                    //验证用户密码
+                    if (administrator.Password != Common.HandleString.MD5(password + administrator.Token))
+                    {
+                        errorCode = 20002;
+                    }
+                }
+                else
+                    errorCode = 20001;
+
+                if (errorCode == 0)
+                {
+                    result = new
+                    {
+                        administrator.Name,
+                        administrator.Token,
+                        administrator.Type,
+                        administrator.Avatar,
+                        Menus = new BLL.ServiceMenu().GetAdminMenu(administrator.ID)
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                errorCode = 10001;
+                new BLL.ServiceException().AddExceptionLog(ex);
+            }
+
+            if (errorCode != 0)
+                error = new BLL.ServiceError().GetErrorInfo(errorCode);
+
+            return Json(errorCode, error, result, true);
+        }
+        #endregion
+
+        #region 图片上传
+        public JsonResult UploadImage(HttpPostedFileBase file)
+        {
+            int errorCode = 0;
+            string error = string.Empty;
+            string result = string.Empty;
+            try
+            {
+                string imgName = string.Format("{0}.jpg", DateTime.Now.ToString("yyyyMMddHHmmss"));
+                string localPath = Server.MapPath("~/Resources/Images/" + imgName);
+                file.SaveAs(localPath);
+                string webPath = string.Format("{0}/Resources/Images/{1}", Common.HandleConfig.GetAppSettingValue("DomainName"), imgName).Replace("/", "\\");
+                result = webPath;
+            }
+            catch (Exception ex)
+            {
+                errorCode = 10001;
+                error = new BLL.ServiceError().GetErrorInfo(errorCode);
+                new BLL.ServiceException().AddExceptionLog(ex);
+            }
+
+
+            return Json(errorCode, error, result, true);
+        }
+        #endregion
+    }
+}
