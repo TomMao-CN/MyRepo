e89f1fff9e254dc4579c9ae2c068fc23167ad4e7
TomMao-CN <53568911+TomMao-CN@users.noreply.github.com>
2020-01-10 17:10:03 +0800

    Delete index.vue

--- Cilent/src/views/页面模板/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="every-page">
-    <div class="toolbar"></div>
-    <div class="content"></div>
-  </div>
-</template>
-<style lang="scss"></style>
-<script src="./index.ts"></script>
