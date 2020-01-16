b907184f8803aefe1cf9bbbaf776064011e41a9e
TomMao-CN <53568911+TomMao-CN@users.noreply.github.com>
2020-01-16 16:26:21 +0800

    Add files via upload

--- WebServer/Common/HandleFiles.cs
+++ WebServer/Common/HandleFiles.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Common
 {
-    class HandleFiles
+    public class HandleFiles
     {
+        #region 检测指定目录是否存在
+        /// <summary>
+        /// 检测指定目录是否存在
+        /// </summary>
+        /// <param name="directoryPath">目录的绝对路径</param>
+        /// <returns></returns>
+        public static bool IsExistDirectory(string directoryPath)
+        {
+            return Directory.Exists(directoryPath);
+        }
+        #endregion
+
+        #region 创建一个目录
+        /// <summary>
+        /// 创建一个目录
+        /// </summary>
+        /// <param name="directoryPath">目录的绝对路径</param>
+        public static void CreateDirectory(string directoryPath)
+        {
+            //如果目录不存在则创建该目录
+            if (!IsExistDirectory(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+        }
+        #endregion
     }
 }
