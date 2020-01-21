003cc9cd459b4cc8424b6ff5aac609ef76a75ae8
TomMao-CN <53568911+TomMao-CN@users.noreply.github.com>
2020-01-21 14:09:23 +0800

    Add files via upload

--- WebServer/Common/HandleHttp.cs
+++ WebServer/Common/HandleHttp.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -38,5 +39,138 @@ namespace Common
             return HttpContext.Current.Request.Form[strName];
         }
         #endregion
+
+        #region 模拟get请求
+        /// <summary>
+        ///  模拟浏览器的get请求
+        /// </summary>
+        /// <param name="url">请求url</param>
+        /// ex:http://api.yizhongchuanmei.comx/Test/Test
+        /// <param name="paramsStr">参数字符串</param>
+        /// ex:test=lnm
+        /// <returns></returns>
+        public static string HttpGet(string url, string paramsStr)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + (paramsStr == "" ? "" : "?") + paramsStr);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            return retString;
+        }
+        #endregion
+
+        #region 模拟post请求
+        /// <summary>
+        /// 模拟浏览器的post请求
+        /// </summary>
+        /// <param name="url">请求url</param>
+        /// ex:http://admin.yizhongchuanmei.comx/Test/Test
+        /// <param name="paramsStr"></param>
+        /// ex:name=lalala&userToken=201907311407320f32fe
+        /// <returns></returns>
+        public static string HttpPost(string url, string paramsStr)
+        {
+            Stream outstream = null;
+            Stream instream = null;
+            StreamReader sr = null;
+            HttpWebResponse response = null;
+            HttpWebRequest request = null;
+            Encoding encoding = System.Text.Encoding.GetEncoding("utf-8");
+            byte[] data = encoding.GetBytes(paramsStr);
+            // 准备请求...
+            try
+            {
+                // 设置参数
+                request = WebRequest.Create(url) as HttpWebRequest;
+                CookieContainer cookieContainer = new CookieContainer();
+                request.CookieContainer = cookieContainer;
+                request.AllowAutoRedirect = true;
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = data.Length;
+                outstream = request.GetRequestStream();
+                outstream.Write(data, 0, data.Length);
+                outstream.Close();
+                //发送请求并获取相应回应数据
+                response = request.GetResponse() as HttpWebResponse;
+                //直到request.GetResponse()程序才开始向目标网页发送Post请求
+                instream = response.GetResponseStream();
+                sr = new StreamReader(instream, encoding);
+                //返回结果网页（html）代码
+                string content = sr.ReadToEnd();
+                string err = string.Empty;
+                return content;
+            }
+            catch (Exception ex)
+            {
+                string err = ex.Message;
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// 模拟httpPost提交表单
+        /// </summary>
+        /// <param name="url">POS请求的网址</param>
+        /// <param name="data">表单里的参数和值</param>
+        /// <param name="encoder">页面编码</param>
+        /// <returns></returns>
+        public static string CreateAutoSubmitForm(string url, Dictionary<string, string> data, Encoding encoder)
+        {
+            StringBuilder html = new StringBuilder();
+            html.AppendLine("<html>");
+            html.AppendLine("<head>");
+            html.AppendFormat("<meta http-equiv=\"Content-Type\" content=\"text/html; charset={0}\" />", encoder.BodyName);
+            html.AppendLine("</head>");
+            html.AppendLine("<body onload=\"OnLoadSubmit();\">");
+            html.AppendFormat("<form id=\"pay_form\" action=\"{0}\" method=\"post\">", url);
+            foreach (KeyValuePair<string, string> kvp in data)
+            {
+                html.AppendFormat("<input type=\"hidden\" name=\"{0}\" id=\"{0}\" value=\"{1}\" />", kvp.Key, kvp.Value);
+            }
+            html.AppendLine("</form>");
+            html.AppendLine("<script type=\"text/javascript\">");
+            html.AppendLine("<!--");
+            html.AppendLine("function OnLoadSubmit()");
+            html.AppendLine("{");
+            html.AppendLine("document.getElementById(\"pay_form\").submit();");
+            html.AppendLine("}");
+            html.AppendLine("//-->");
+            html.AppendLine("</script>");
+            html.AppendLine("</body>");
+            html.AppendLine("</html>");
+            return html.ToString();
+        }
+        #endregion
+
+        #region IP相关
+        public static string GetRealIp()
+        {
+            string userHostAddress = HttpContext.Current.Request.UserHostAddress;
+
+            if (string.IsNullOrEmpty(userHostAddress))
+                userHostAddress = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+
+            //最后判断获取是否成功，并检查IP地址的格式（检查其格式非常重要）
+            if (!string.IsNullOrEmpty(userHostAddress) && IsIP(userHostAddress))
+            {
+                return userHostAddress;
+            }
+            return "0.0.0.0";
+        }
+
+        public static bool IsIP(string ip)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(ip, @"^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$");
+        }
+        #endregion
+
     }
 }
--- WebServer/Common/HandleJson.cs
+++ WebServer/Common/HandleJson.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace Common
 {
@@ -15,5 +17,52 @@ namespace Common
             return JsonConvert.DeserializeAnonymousType(json, anonymousTypeObject);
         }
         #endregion
+
+        #region Json数据转化为动态类型
+        public static dynamic JsonToDynamic(string json)
+        {
+            return JToken.Parse(json) as dynamic;
+        }
+        #endregion
+
+        #region 解析JSON字符串生成对象实体
+
+        /// <summary>
+        /// 解析JSON字符串生成对象实体
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="json">json字符串(eg.{"ID":"112","Name":"石子儿"})</param>
+        /// <returns>对象实体</returns>
+        public static T DeserializeJsonToObject<T>(string json) where T : class
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            StringReader sr = new StringReader(json);
+            object o = serializer.Deserialize(new JsonTextReader(sr), typeof(T));
+            T t = o as T;
+            return t;
+        }
+
+        #endregion
+
+        #region 解析JSON数组生成对象实体集合
+
+        /// <summary>
+        /// 解析JSON数组生成对象实体集合
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="json">json数组字符串(eg.[{"ID":"112","Name":"石子儿"}])</param>
+        /// <returns>对象实体集合</returns>
+        public static List<T> DeserializeJsonToList<T>(string json) where T : class
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            StringReader sr = new StringReader(json);
+            object o = serializer.Deserialize(new JsonTextReader(sr), typeof(List<T>));
+            List<T> list = o as List<T>;
+            return list;
+        }
+
+        #endregion
+
+
     }
 }
