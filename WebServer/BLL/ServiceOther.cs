d07e7bac6fb353b77c2eceec03ea3c410771b8a1
TomMao-CN <53568911+TomMao-CN@users.noreply.github.com>
2020-01-21 14:08:06 +0800

    Add files via upload

---
+++ WebServer/BLL/ServiceOther.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ServiceOther:SuperClass
+    {
+        public string GetTemperature(string input)
+        {
+            string pattern = @"-?[\d]+[.]?[\d]*";
+            Match match = Regex.Match(input, pattern);
+            return match.Value;
+        }
+    }
+}
